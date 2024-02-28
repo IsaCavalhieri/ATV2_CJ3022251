54270d2026bbc459f20c78717a4c93769ced585d
CJ3022251 <CJ3022251@LAB01-PC37>
2024-02-28 08:55:37 -0300

    Adicionar arquivos de projeto.

---
+++ ATV2_CJ3022251/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ATV2_CJ3022251
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float salario;
+
+            Console.WriteLine("escreva seu salário :");
+
+            salario = float.Parse(Console.ReadLine());
+
+            salario = salario + (float)(25.0f / 100.0f) * salario;
+
+            Console.WriteLine("seu novo salário é : {0}", salario);
+               
+
+
+           
+        }
+    }
+}
